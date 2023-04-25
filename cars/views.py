2fe4dee358fd97d17559f8de250d6f4d7d9fbb1f
Divyansh <divyansh.s@cisinlabs.com>
2023-04-25 16:09:57 +0530

    filteration done

--- cars/views.py
+++ cars/views.py
@@ -7,11 +7,24 @@ from .models import NewCars, UsedCars
 def NewCarViews(request):
     # for sort filter
     ordering = request.GET.get('ordering', "")
+    exshowroom_price = request.GET.get('exshowroom_price', "")
+    body_type = request.GET.get('body_type', "")
+    milege = request.GET.get('milege', "")
     cars = NewCars.objects.all()
     
     if ordering:
         cars = cars.order_by(ordering)
+        print(cars)
 
+    if exshowroom_price:
+        cars = cars.filter(exshowroom_price__lt = exshowroom_price)
+
+    if body_type:
+        cars = cars.filter(body_type = body_type)
+
+    if milege:
+        cars = cars.filter(milege__lt = milege)
+        print(cars)
     # for pagination
     page_number = request.GET.get('page', 1)
     
@@ -31,10 +44,24 @@ def NewCarViews(request):
 def UsedCarViews(request):
     # for sort filter
     ordering = request.GET.get('ordering', "")
+    price = request.GET.get('price', "")
+    kilometer_run = request.GET.get('kilometer_run', "")
+    buy_year = request.GET.get('buy_year', "")
+  
     cars = UsedCars.objects.all()
     if ordering:
         cars = cars.order_by(ordering)
+    
+    if price:
+        cars = cars.filter(demand__lt = price)
         
+    if kilometer_run:
+        cars = cars.filter(kilometer_run__lt = kilometer_run)
+
+    if buy_year:
+        cars = cars.filter(buy_year = buy_year)
+        
+    
     page_number = request.GET.get('page', 1)
     
     p = Paginator(cars,3)
